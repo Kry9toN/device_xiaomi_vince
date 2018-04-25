c5ea736c2e07dd55b6917b4323129fa0cf0885d9
Adesh15 <adesikha15@gmail.com>
2018-04-25 11:54:03 +0000

    msm8953-common: New DPI(420)
    Signed-off-by: Adesh15 <adesikha15@gmail.com>

--- prop.mk
+++ prop.mk
@@ -118,7 +118,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
 debug.sf.early_phase_offset_ns=1500000 \
 debug.sf.early_app_phase_offset_ns=1500000 \
 debug.sf.early_gl_phase_offset_ns=3000000 \
-debug.sf.early_gl_app_phase_offset_ns=15000000
+debug.sf.early_gl_app_phase_offset_ns=15000000 \
+ro.sf.lcd_density=420
 
 # DRM
 PRODUCT_PROPERTY_OVERRIDES += \
