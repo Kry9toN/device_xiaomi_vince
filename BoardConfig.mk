41b39a04674337caff36dc603123af36ebcd8676
RevanthTolety <crevanthtolety@gmail.com>
2020-03-08 01:18:03 +0530

    vince: Ignore Selinux Neverallows
    Signed-off-by: sreekfreak995 <sreekfreak995@gmail.com>
    Signed-off-by: Kry9toN <dhimasbagusprayoga@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -209,7 +209,10 @@ endif
 include device/qcom/sepolicy-legacy-um/sepolicy.mk
 BOARD_SEPOLICY_DIRS += $(DEVICE_PATH)/sepolicy
 
+# We modify several neverallows, so let the build proceed
+ifneq ($(TARGET_BUILD_VARIANT),user)
 SELINUX_IGNORE_NEVERALLOWS := true
+endif
 
 # Wi-Fi
 BOARD_HAS_QCOM_WLAN := true
